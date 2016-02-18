13c8782bddbe03eee05418475704e4896e5c2a68
Nic Netzel and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-18 14:15:09 -0800

    Add initial project files

---
+++ tests/FindWeekday.php
@@ -0,0 +1,11 @@
+<?php
+require_once "src/Weekday.php";
+
+class  FindWeekdayTest  extends PHPUnit_Framework_TestCase{
+
+    function test_Find_Today()
+    {
+        $test_FindWeekday = new FindWeekday
+    }
+}
+ ?>
